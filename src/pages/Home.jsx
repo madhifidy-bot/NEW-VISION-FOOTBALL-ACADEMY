f3ce865b997dfad321562abd1e796198720307d0
Your Name <your-email@example.com>
2026-05-05 15:07:33 +0300

    Fix: correct case sensitivity for Home folder imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Hero from '../components/home/Hero';
-import Carousel from '../components/home/Carousel';
-import AboutPreview from '../components/home/AboutPreview';
-import ProgramsPreview from '../components/home/ProgramsPreview';
-import NextMatch from '../components/home/NextMatch';
-import ScheduleHonours from '../components/home/ScheduleHonours';
-import Partners from '../components/home/Partners';
-import Newsletter from '../components/home/Newsletter';
+import Hero from '../components/Home/Hero';
+import Carousel from '../components/Home/Carousel';
+import AboutPreview from '../components/Home/AboutPreview';
+import ProgramsPreview from '../components/Home/ProgramsPreview';
+import NextMatch from '../components/Home/NextMatch';
+import ScheduleHonours from '../components/Home/ScheduleHonours';
+import Partners from '../components/Home/Partners';
+import Newsletter from '../components/Home/Newsletter';
 
 const Home = () => {
   console.log('[Home] Full Bayern‑style homepage loaded');
